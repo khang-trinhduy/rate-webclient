38a95a97c67f55ac7749684afb6e5ccd936a4a20
Khang Duy <trinhduykhang1313@gmail.com>
2020-02-26 14:02:52 +0700

    update hide settings, add format date

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,16 +1,22 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
 
 @Component({
-  selector: 'app-card',
-  templateUrl: './card.component.html',
-  styleUrls: ['./card.component.sass']
+  selector: "app-card",
+  templateUrl: "./card.component.html",
+  styleUrls: ["./card.component.sass"]
 })
 export class CardComponent implements OnInit {
   @Input() rate;
   @Input() gifts;
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  toDate(date) {
+    return date
+      .split("T")[0]
+      .split("-")
+      .reverse()
+      .join("/");
   }
 
+  ngOnInit() {}
 }
