0003cd559b323036d6832946ddc21948e06a6785
Khang Duy <trinhduykhang1313@gmail.com>
2020-02-26 18:17:55 +0700

    update user review

--- src/app/components/review/review.component.ts
+++ src/app/components/review/review.component.ts
@@ -14,11 +14,15 @@ export class ReviewComponent implements OnInit {
 
   ngOnInit() {}
 
-  getTwinkleWidth = stars => {
+  getTwinkleWidth = (stars, small = false) => {
+    if (small) {
+      return `${Math.floor((120 * stars) / 5)}px`;
+    }
     return `${Math.floor((173 * stars) / 5)}px`;
   };
 
-  getHelperWidth = (stars: number) => {
+  getHelperWidth = (stars, small = false) => {
+    if (small) return `${Math.floor((120 * Math.floor(5 - stars)) / 5)}px`;
     return `${Math.floor((173 * Math.floor(5 - stars)) / 5)}px`;
   };
 
