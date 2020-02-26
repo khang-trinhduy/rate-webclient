dd01f9fde22223f1035cadef51a25c88e31b831f
Khang Duy <trinhduykhang1313@gmail.com>
2020-02-26 17:55:34 +0700

    summary area ok

--- src/app/components/review/review.component.ts
+++ src/app/components/review/review.component.ts
@@ -15,11 +15,11 @@ export class ReviewComponent implements OnInit {
   ngOnInit() {}
 
   getTwinkleWidth = stars => {
-    return `${Math.floor((120 * stars) / 5)}px`;
+    return `${Math.floor((173 * stars) / 5)}px`;
   };
 
   getHelperWidth = (stars: number) => {
-    return `${Math.floor((120 * Math.floor(5 - stars)) / 5)}px`;
+    return `${Math.floor((173 * Math.floor(5 - stars)) / 5)}px`;
   };
 
   toDate(date) {
