a64f547600b58bc84a6d3a7cedd9fe878290bf4e
Khang Duy <trinhduykhang1313@gmail.com>
2020-02-28 14:13:14 +0700

    add gift

--- src/app/layout/add-gift/add-gift.component.ts
+++ src/app/layout/add-gift/add-gift.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-add-gift',
-  templateUrl: './add-gift.component.html',
-  styleUrls: ['./add-gift.component.sass']
+  selector: "app-add-gift",
+  templateUrl: "./add-gift.component.html",
+  styleUrls: ["./add-gift.component.sass"]
 })
 export class AddGiftComponent implements OnInit {
+  form = this.fb.group({
+    name: ["", Validators.required],
+    date: ["", Validators.required],
+    rate: ["", Validators.required]
+  });
+  constructor(public fb: FormBuilder) {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
