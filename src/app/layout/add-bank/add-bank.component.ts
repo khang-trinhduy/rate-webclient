0e2d2e3a447e4b37673e35b7b6e61c207b35038d
Khang Duy <trinhduykhang1313@gmail.com>
2020-02-28 13:45:28 +0700

    add bank

--- src/app/layout/add-bank/add-bank.component.ts
+++ src/app/layout/add-bank/add-bank.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-add-bank',
-  templateUrl: './add-bank.component.html',
-  styleUrls: ['./add-bank.component.sass']
+  selector: "app-add-bank",
+  templateUrl: "./add-bank.component.html",
+  styleUrls: ["./add-bank.component.sass"]
 })
 export class AddBankComponent implements OnInit {
+  form = this.fb.group({
+    tradeName: ["", Validators.required],
+    nativeName: ["", Validators.required],
+    webiste: [""],
+    headquater: [""],
+    founded: [""],
+    type: ["", Validators.required],
+    logo: [""]
+  });
+  constructor(public fb: FormBuilder) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  save() {
+
   }
-
 }
