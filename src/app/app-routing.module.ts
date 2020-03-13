97a3f3883ddf0e062c85c344f2e032ba59a5834e
Khang Duy <trinhduykhang1313@gmail.com>
2020-03-13 16:39:55 +0700

    even more bs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { InterestComponent } from "./layout/interest/interest.component";
-import { HomeComponent } from "./layout/home/home.component";
 import { DetailComponent } from "./layout/detail/detail.component";
 import { LoginComponent } from "./layout/login/login.component";
 import { RegisterComponent } from "./layout/register/register.component";
+import { SubscribeComponent } from './layout/subscribe/subscribe.component';
 
 const routes: Routes = [
   { path: "", component: InterestComponent },
   { path: "detail", component: DetailComponent },
   { path: "signin", component: LoginComponent },
-  { path: "signup", component: RegisterComponent }
+  { path: "signup", component: RegisterComponent },
+  { path: "register", component: SubscribeComponent }
 ];
 
 @NgModule({
