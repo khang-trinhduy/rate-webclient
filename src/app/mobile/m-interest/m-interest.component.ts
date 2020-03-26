8e2cb7934fc0dbe15f65f0bd3981c60b80fb5a6c
Khang Duy <trinhduykhang1313@gmail.com>
2020-03-26 18:13:22 +0700

    add loader to m-interest

--- src/app/mobile/m-interest/m-interest.component.ts
+++ src/app/mobile/m-interest/m-interest.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,9 +8,16 @@ import { Router } from '@angular/router';
 })
 export class MInterestComponent implements OnInit {
 
+  @ViewChild("loader", { static: false }) loader: ElementRef;
+
   constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    let loader = this.loader.nativeElement;
+    (<HTMLElement>loader).classList.add('mkdih');
   }
 
   recommend = () => this.router.navigateByUrl("recommend");
