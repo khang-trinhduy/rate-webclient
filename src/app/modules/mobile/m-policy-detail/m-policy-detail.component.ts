b7927c77b7e3f10828e6e190d0e68794e906cf08
Khang Duy <trinhduykhang1313@gmail.com>
2020-04-09 16:41:53 +0700

    update policy

--- src/app/modules/mobile/m-policy-detail/m-policy-detail.component.ts
+++ src/app/modules/mobile/m-policy-detail/m-policy-detail.component.ts
@@ -1,16 +1,17 @@
-import { Component, OnInit, Inject } from "@angular/core";
-import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material/dialog";
+import { Component, OnInit, Inject } from '@angular/core'
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog'
 
 @Component({
-  selector: "app-m-policy-detail",
-  templateUrl: "./m-policy-detail.component.html",
-  styleUrls: ["./m-policy-detail.component.sass"]
+  selector: 'app-m-policy-detail',
+  templateUrl: './m-policy-detail.component.html',
+  styleUrls: ['./m-policy-detail.component.sass'],
 })
 export class MPolicyDetailComponent implements OnInit {
-  vib = false;
-  scb = false;
-  abb = false;
-  tcb = false;
+  vib = false
+  scb = false
+  abb = false
+  tcb = false
+  pvc = false
 
   constructor(
     public dialogRef: MatDialogRef<MPolicyDetailComponent>,
@@ -18,15 +19,17 @@ export class MPolicyDetailComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    let bank = this.data.bank.toString();
-    if (bank.toLowerCase() === "vib") {
-      this.vib = true;
-    } else if (bank.toLowerCase() === "abbank") {
-      this.abb = true;
-    } else if (bank.toLowerCase() === "techcombank") {
-      this.tcb = true;
+    let bank = this.data.bank.toString()
+    if (bank.toLowerCase() === 'vib') {
+      this.vib = true
+    } else if (bank.toLowerCase() === 'abbank') {
+      this.abb = true
+    } else if (bank.toLowerCase() === 'techcombank') {
+      this.tcb = true
+    } else if (bank.toLowerCase() === 'pvcombank') {
+      this.pvc = true
     } else {
-      this.scb = true;
+      this.scb = true
     }
   }
 }
--- src/app/modules/mobile/m-policy/m-policy.component.ts
+++ src/app/modules/mobile/m-policy/m-policy.component.ts
@@ -1,63 +1,68 @@
-import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
-import { Logos } from "src/app/models/banks";
-import { MatDialog } from "@angular/material/dialog";
-import { MPolicyDetailComponent } from "../m-policy-detail/m-policy-detail.component";
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core'
+import { Logos } from 'src/app/models/banks'
+import { MatDialog } from '@angular/material/dialog'
+import { MPolicyDetailComponent } from '../m-policy-detail/m-policy-detail.component'
 
 @Component({
-  selector: "app-m-policy",
-  templateUrl: "./m-policy.component.html",
-  styleUrls: ["./m-policy.component.sass"]
+  selector: 'app-m-policy',
+  templateUrl: './m-policy.component.html',
+  styleUrls: ['./m-policy.component.sass'],
 })
 export class MPolicyComponent implements OnInit {
   policys = [
     {
-      bank: "techcombank",
-      title: "Techcombank cộng thêm 0.2% cho Khách hàng VIP",
-      date: "23/03/2020"
+      bank: 'techcombank',
+      title: 'Techcombank cộng thêm 0.2% cho Khách hàng VIP',
+      date: '23/03/2020',
     },
     {
-      bank: "abbank",
-      title: "Ngân hàng ABBank mới cập lãi suất tháng 3/2020",
-      date: "20/03/2020"
+      bank: 'abbank',
+      title: 'Ngân hàng ABBank mới cập lãi suất tháng 3/2020',
+      date: '20/03/2020',
     },
     {
-      bank: "scb",
-      title: "Chính sách cộng lãi suất tốt nhất từ SCB",
-      date: "18/03/2020"
+      bank: 'scb',
+      title: 'Chính sách cộng lãi suất tốt nhất từ SCB',
+      date: '18/03/2020',
     },
     {
-      bank: "vib",
-      title: "Lãi suất cao nhất tại VIB",
-      date: "18/03/2020"
-    }
-  ];
+      bank: 'vib',
+      title: 'Lãi suất cao nhất tại VIB',
+      date: '18/03/2020',
+    },
+    {
+      bank: 'pvcombank',
+      title: 'Lãi suất tiền gửi Online PVcombank cao hơn 0.3% lãi suất tiền gửi tại quầy',
+      date: '09/04/2020',
+    },
+  ]
 
-  @ViewChild("loader", { static: false }) loader: ElementRef;
+  @ViewChild('loader', { static: false }) loader: ElementRef
 
   constructor(private dialog: MatDialog) {}
 
   ngOnInit() {}
 
   ngAfterViewInit(): void {
-    let loader = this.loader.nativeElement;
-    (<HTMLElement>loader).classList.add("mkdih");
+    let loader = this.loader.nativeElement
+    ;(<HTMLElement>loader).classList.add('mkdih')
   }
 
-  detail = bank => {
+  detail = (bank) => {
     this.dialog.open(MPolicyDetailComponent, {
-      width: "87.5vw",
-      height: "80vh",
+      width: '87.5vw',
+      height: '80vh',
       data: {
-        bank: bank
-      }
-    });
-  };
+        bank: bank,
+      },
+    })
+  }
 
   getLogo(code) {
     if (code) {
-      let bank = new Logos();
-      code = code.split(" ")[0].toLowerCase();
-      return bank[code];
+      let bank = new Logos()
+      code = code.split(' ')[0].toLowerCase()
+      return bank[code]
     }
   }
 }
